d3610c26b84effb98e43e28d5a91ad77a41bda26
SIMMACO FERRIERO <af12479>
2023-01-10 07:42:23 +0000

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("my.company.bupasprj.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
